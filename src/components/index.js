0d7d9d1096cffb973084a1c6c412f1c87b4d6b66
Anton <anm19911005@gmail.com>
2024-06-28 19:17:35 +0300

    Revert "add filtersSlice"
    This reverts commit 22c2a6da8c08894a0fd24ae6fa59731ecc216a40.

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./Contact/Contact";
-export * from "./ContactForm/ContactForm";
-export * from "./ContactList/ContactList";
-export * from "./SearchBox/SearchBox";
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
-import App from "./App.jsx";
-import store from "./redux/store.js";
+import App from "./components/App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </Provider>
+  </BrowserRouter>
 );
